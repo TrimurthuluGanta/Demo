f921a33b913ec42784811810c453824db6305eeb
TrimurthuluGanta <trimurthuluganta22@gmail.com>
2022-04-12 20:24:53 +0530

    added new ts file

---
+++ child.component.ts
@@ -0,0 +1,20 @@
+import { EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+  @Input() PData:any;
+  @Output() childEvent=new EventEmitter();
+  constructor() { }
+  onChange(value){
+    this.childEvent.emit(value);
+  }
+  ngOnInit(): void {
+  }
+
+}
